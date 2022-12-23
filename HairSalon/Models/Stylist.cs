4cc0fc684dbebf29e96b2dbc7854387c7bebf63a
Chris Loveless <chriskloveless@gmail.com>
2022-12-23 09:05:37 -0800

    add splash page and Stylist Index page

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,9 @@
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        public int StylistId { get; set; }
+        public string Name { get;  set; }
+        public List<Client> Clients { get; set; }
+    }
+}
