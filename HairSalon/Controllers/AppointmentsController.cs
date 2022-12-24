8840f09a3ed48b6317976437aaeacd1a053adb56
Chris Loveless <chriskloveless@gmail.com>
2022-12-23 16:04:50 -0800

    add edit, delete and details page for appointments

--- HairSalon/Controllers/AppointmentsController.cs
+++ HairSalon/Controllers/AppointmentsController.cs
@@ -37,5 +37,41 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Delete(int id)
+    {
+      Appointment thisAppointment = _db.Appointments.FirstOrDefault(appointment => appointment.AppointmentId == id);
+      return View(thisAppointment);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Appointment thisAppointment = _db.Appointments.FirstOrDefault(appointment => appointment.AppointmentId == id);
+      _db.Appointments.Remove(thisAppointment);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Appointment thisAppointment = _db.Appointments.Include(appointment => appointment.Client).FirstOrDefault(appointment => appointment.AppointmentId == id);
+      return View(thisAppointment);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Appointment thisAppointment = _db.Appointments.FirstOrDefault(resraurant => resraurant.AppointmentId == id);
+      ViewBag.ClientId = new SelectList(_db.Clients, "ClientId", "Name");
+      return View(thisAppointment);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Appointment appointment)
+    {
+      _db.Appointments.Update(appointment);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
